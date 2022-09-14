18afb5d4ef9160fb097b4be98f708dfa6b48e611
Milos Petrovic <milos.brugi@gmail.com>
2022-09-14 10:28:33 -0400

    Undo alembic fly update

--- app/db.py
+++ app/db.py
@@ -3,8 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-print(environ.get("FAAUTH_DB"))
-
 engine = create_engine(environ.get("FAAUTH_DB"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
